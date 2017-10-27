7fcf57da677aec85e7999d5d8350a0f0cea693c8
tonytong <tongwanhua1993@163.com>
2017-10-27 11:47:08 +0800

    搭建动画的舞台

--- TYAnimationDemo/TYAnimationDemo/ViewController.swift
+++ TYAnimationDemo/TYAnimationDemo/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var heading: UILabel!
+    @IBOutlet var username: UITextField!
+    @IBOutlet var password: UITextField!
+    
+    @IBOutlet var cloud1: UIImageView!
+    @IBOutlet var cloud2: UIImageView!
+    @IBOutlet var cloud3: UIImageView!
+    @IBOutlet var cloud4: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        loginButton.layer.cornerRadius = 8.0
+        loginButton.layer.masksToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
